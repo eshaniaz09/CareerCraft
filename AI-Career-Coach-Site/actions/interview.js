f1a20ffd2b0638bce727d0521f173dfd73e6ed53
eshaniaz9 <esha_niaz_intern@netixsol.com>
2026-05-07 13:58:59 +0500

    interview update

--- AI-Career-Coach-Site/actions/interview.js
+++ AI-Career-Coach-Site/actions/interview.js
@@ -22,11 +22,9 @@ export async function generateQuiz() {
   if (!user) throw new Error("User not found");
 
   const prompt = `
-    Generate 3 technical interview questions for a ${
-      user.industry
-    } professional${
-    user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""
-  }.
+    Generate 10 technical interview questions for a ${user.industry
+    } professional${user.skills?.length ? ` with expertise in ${user.skills.join(", ")}` : ""
+    }.
     
     Each question should be multiple choice with 4 options.
     
